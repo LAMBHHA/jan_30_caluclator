b37d9daa6f45682a1504a210ead499440cc8a20c
Gurnam Lambhha <lambhha@sheridancollege.ca>
2019-01-30 22:30:07 -0500

    Example of subtraction using TDD

--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -6,13 +6,17 @@ from calculator import subtract
 
 def test_five_minus_two():
     """
-    If given 2 and 2 as parameters, 4 should
+    If given 5 and 2 as parameters, 3 should
     be returned
     """
     assert subtract(5, 2) == 3
 
 def test_eight_minus_three():
     assert subtract(8, 3) == 5
+    """
+    If given 8 and 3 as parameters, 5 should
+    be returned
+    """
 
 def test_no_parameter():
     """ if no parameters are provided, return 0
